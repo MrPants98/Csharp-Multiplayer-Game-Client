997758c6f292ebf72172697934c5b3a52194e37f
MrPants98 <114786513+MrPants98@users.noreply.github.com>
2024-09-11 19:39:57 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Raylib_cs;
+
+namespace Csharp_Multiplayer_Game_Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(1280, 720, "Multiplayer Game");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.White);
+
+                Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
